be4df3cd0772f07014398550d111b4a7ebb74176
Aliaksei Bychyk <Aliaksei_Bychyk@epam.com>
2018-06-04 16:19:21 +0300

    init commit

---
+++ server/service.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const service = express();
+
+service.get('/service/:location', (req, res, next) => {
+  res.json({result: req.params.location});
+});
+
+module.exports = service;
