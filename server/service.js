14b5e8469d44b203006031995f33bace50438c3c
Aliaksei Bychyk <Aliaksei_Bychyk@epam.com>
2018-06-04 17:49:37 +0300

    google geolocation integration

--- server/service.js
+++ server/service.js
@@ -1,9 +1,26 @@
 const express = require('express');
+const request = require('superagent');
+const googleMapApiKey = require('../secret/googleMapApiKey');
 
 const service = express();
 
+service.use(express.json());
+
+
 service.get('/service/:location', (req, res, next) => {
-  res.json({result: req.params.location});
+  const locationAddress = req.params.location;
+  const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${locationAddress}&key=${googleMapApiKey}`;
+
+  request.get(url, (err, response) => {
+    if (err) {
+      console.error(err);
+      return res.sendStatus(500);
+    }
+
+    res.json(response.body);
+    // res.json(response.body.results[0].geometry.location);
+
+  });
 });
 
 module.exports = service;
