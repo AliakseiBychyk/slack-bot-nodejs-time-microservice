c566ae8b0cdf8ee0c77623ae7dbbf26d59d54cf6
Aliaksei Bychyk <Aliaksei_Bychyk@epam.com>
2018-06-05 14:22:25 +0300

    wire up time microservice

--- server/service.js
+++ server/service.js
@@ -1,6 +1,7 @@
 const express = require('express');
+const moment = require('moment');
 const request = require('superagent');
-const googleMapApiKey = require('../secret/googleMapApiKey');
+const {timeZoneApiKey, geocodingApiKey} = require('../secret/googleMapApiKey');
 
 const service = express();
 
@@ -9,17 +10,32 @@ service.use(express.json());
 
 service.get('/service/:location', (req, res, next) => {
   const locationAddress = req.params.location;
-  const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${locationAddress}&key=${googleMapApiKey}`;
 
-  request.get(url, (err, response) => {
+  const geocodeUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${locationAddress}&key=${geocodingApiKey}`;
+
+  request.get(geocodeUrl, (err, response) => {
     if (err) {
       console.error(err);
       return res.sendStatus(500);
     }
 
-    res.json(response.body);
-    // res.json(response.body.results[0].geometry.location);
+    const location = response.body.results[0].geometry.location;
+    const {lat, lng} = location;
 
+    const timestamp = +moment().format('X');
+
+    const timezoneUrl = `https://maps.googleapis.com/maps/api/timezone/json?location=${lat},${lng}&timestamp=${timestamp}&key=${timeZoneApiKey}`;
+    request.get(timezoneUrl, (err, response) => {
+      if (err) {
+        console.error(err);
+        return res.sendStatus(500);
+      }
+
+      const result = response.body;
+      const timeString = moment.unix(timestamp + result.dstOffset + result.rawOffset).utc().format('dddd, MMMM, Do YYYY, h:mm:ss a');
+
+      res.json({result: timeString});
+    });
   });
 });
 
